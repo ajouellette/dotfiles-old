6c9438ccddfe9ffc2ba328cbce85d3135d544bb2
aouelete <aouellette2016@gmail.com>
2015-08-13 20:37:49 -0400

    volume change script

---
+++ scripts/volume.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# z3bra - (c) wtfpl 2014
+# Manage ALSA Master channel
+
+test "$1" = "-h" && echo "usage `basename $0` [+|-|!]" && exit 0
+
+level() {
+    amixer get Master | sed -n 's/^.*\[\([0-9]\+%\).*$/\1/p' | uniq
+}
+
+state() {
+    amixer get Master | sed -n 's/^.*\[\(o[nf]\+\)]$/\1/p' | uniq
+}
+
+test $# -eq 0 && echo "`level` `state`" && exit 0
+
+case $1 in
+    +)      amixer set Master 3%+ >/dev/null;;
+    -)      amixer set Master 3%- >/dev/null;;
+    !)      amixer set Master toggle >/dev/null;;
+    state|level) $1;;
+    *)    amixer set Master $1 >/dev/null;;
+esac
