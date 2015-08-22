54fc607fd6f0392e7a0de6c7ce04db490d981564
aouelete <aouellette2016@gmail.com>
2015-08-21 20:33:07 -0400

    allow terminal to be specified on cmd line

--- scripts/drawterm.sh
+++ scripts/drawterm.sh
@@ -2,6 +2,8 @@
 # select a rectangle and launch a floating terminal
 # needs slop
 
+# specify terminal on cmd line (default urxvt)
+term=${1:-urxvt}
 
 REC=$(slop -t 0 -b 2 -c 1,1,1,1 -f "%w %h %x %y" ) || exit 1
 
@@ -15,7 +17,7 @@ if [ "$W" -gt "1" ]; then
     let W=$(( $W / 7 ))-2
     let H=$(( $H / 15 ))-1
 
-    xfce4-terminal --geometry $W"x"$H"+"$X"+"$Y -T "floating (term)" &
+    eval "$term --geometry $W\"x\"$H\"+\"$X\"+\"$Y -T \"floating (Terminal)\"" &
 fi
 
 exit 0
