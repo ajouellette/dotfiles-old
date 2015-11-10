fc115c76a5ea65e03f0557f4ba02a4bc5760c4cc
Aaron Ouellette <aouellette2016@gmail.com>
2015-11-10 17:31:47 -0500

    make config dir if not present

--- stow.sh
+++ stow.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 display_help() {
-    echo "Usage: [re|un]stow.sh <package1> [<package2> <package3> ...]" 
+    echo "Usage: [re|un]stow.sh <package1> [<package2> <package3> ...]"
     echo "    stow.sh:   creates symlinks to dotfiles for specified programs"
     echo "    unstow.sh: removes symlinks"
     echo "    restow.sh: removes and then re-creates symlinks"
-    echo 
+    echo
     echo "    Stow determines what directories the symlinks go in"
     echo "    by parsing the file \"index\""
 }
@@ -21,17 +21,18 @@ main() {
         *stow*)   flag=--stow;;
         *)        echo "Error!"; return 1;;
     esac
-        
+
     for package in $@; do
         if [ -d "$package" ]; then
             array=( `grep "^$package:" index` )
-        
+
             if [ -z "$array" ]; then
                 echo "Error: \"$package\" not found in index"
                 return 1
             fi
-        
+
             dir=${array[1]}
+            eval "mkdir -p $dir"
             eval "stow --verbose=1 -t $dir $flag $package"
         else
             echo "Error: directory \"$package\" not found"
